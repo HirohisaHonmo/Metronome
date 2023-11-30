48f8191f9b614628316b55c09e5ab22aa9e0fffa
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2023-11-30 23:54:30 +0900

    マジックナンバーにしていたサンプルレートを定数化

--- generate_tone.py
+++ generate_tone.py
@@ -2,7 +2,9 @@ import numpy as np
 from scipy.io.wavfile import write
 
 
-def generate_tone(frequency, duration, sample_rate=44100, volume=0.5):
+SAMPLE_RATE = 44100
+
+def generate_tone(frequency, duration, sample_rate=SAMPLE_RATE, volume=0.5):
     num_samples = int(sample_rate * duration)
 
     t = np.linspace(0, duration, num_samples, False)
@@ -16,16 +18,16 @@ def generate_tone(frequency, duration, sample_rate=44100, volume=0.5):
 A_FREQUENCY = 440
 A_DURATION = 2
 a_sound = generate_tone(A_FREQUENCY, A_DURATION)
-write("Sound/A440.wav", 44100, a_sound)
+write("Sound/A440.wav", SAMPLE_RATE, a_sound)
 
 # メトロノームの低い音
 LOW_CLICK_FREQUENCY = 880
 LOW_CLICK_DURATION = 0.1
 low_click_sound = generate_tone(LOW_CLICK_FREQUENCY, LOW_CLICK_DURATION)
-write("Sound/click_low.wav", 44100, low_click_sound)
+write("Sound/click_low.wav", SAMPLE_RATE, low_click_sound)
 
 # メトロノームの高い音
 HIGH_CLICK_FREQUENCY = 1760
 HIGH_CLICK_DURATION = 0.1
 high_click_sound = generate_tone(HIGH_CLICK_FREQUENCY, HIGH_CLICK_DURATION)
-write("Sound/click_high.wav", 44100, high_click_sound)
+write("Sound/click_high.wav", SAMPLE_RATE, high_click_sound)
